6a5f17be45388f4bc21b07809cdf1feb5eaefe9c
Eduardo Poblete <eduardopoblete1998@gmail.com>
2021-03-09 10:16:17 -0300

    docs agregado

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+
+export const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=oOu0omYMpd4RYBX1RQLZABOdqZcwITVx`;
+    const respuesta = await fetch(url)
+    const {data} = await respuesta.json();
+
+    const gifs = data.map( img => {
+        return{
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+    
+}
